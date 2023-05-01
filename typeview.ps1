6124e913b1d5848f4558183b04f6e05057e113d3
neuralpain <77242216+neuralpain@users.noreply.github.com>
2023-05-01 13:47:45 -0400

    v0.2

--- typeview.ps1
+++ typeview.ps1
@@ -1,18 +1,35 @@
-param([string] $Directory)
+<#
+  typeview.ps1, Version 0.2
+  Copyright (c) 2023, neuralpain
+  View your local typefaces in the browser
+#>
+
+param([Parameter(Mandatory)][string]$Directory, [string]$Update)
 
 # if (-not(Test-Path "$Directory\_fontwebview")) { mkdir "$Directory\_fontwebview" }
 
-$FONT_DIR = $Directory
-$FONT_WEBVIEW = $Directory
-# $FONT_WEBVIEW = "$Directory\_fontwebview"
-$FONT_WEBVIEW_INDEX = "$FONT_WEBVIEW\index.html"
-$FONT_WEBVIEW_CSS = "$FONT_WEBVIEW\fontcache.css"
-$FDIR_LIST = "$FONT_WEBVIEW\DIRLIST.CSV"
-$FONT_LIST = "$FONT_WEBVIEW\FONTLIST.CSV"
-
-# clean up endings
+# clean up slash endings
 while (($Directory.Substring($Directory.Length - 1) -eq "\") -or ($Directory.Substring($Directory.Length - 1) -eq "/")) {
   $Directory = $Directory.Substring(0, $Directory.Length - 1)
+}
+
+if (-not(Test-Path $Directory)) { Write-Host "typeview: Directory does not exist."; exit }
+
+# $FONT_WEBVIEW = "$Directory\_fontwebview" # Re: `Clear-FontCache`
+$FONT_WEBVIEW_INDEX = "$Directory\index.html"
+$FONT_WEBVIEW_CSS = "$Directory\fontcache.css"
+# $FDIR_LIST = "$Directory\DIRLIST.CSV" 
+$FONT_LIST = "$Directory\FONTLIST.CSV"
+
+Write-Host;
+
+function Remove-Typeview {
+  if ((Test-Path $FDIR_LIST) -or (Test-Path $FONT_LIST) -or (Test-Path $FONT_WEBVIEW_INDEX) -or (Test-Path $FONT_WEBVIEW_CSS)) {
+    # Remove-Item $FDIR_LIST >$null 2>&1
+    Remove-Item $FONT_LIST >$null 2>&1
+    Remove-Item $FONT_WEBVIEW_INDEX >$null 2>&1
+    Remove-Item $FONT_WEBVIEW_CSS >$null 2>&1
+  }
 }
 
 function RemoveQuotesFromPath {
@@ -23,43 +40,57 @@ function RemoveQuotesFromPath {
   Set-Content -Path $File
 }
 
-function Clear-FontCache { 
-  if (Test-Path $FDIR_LIST) { Clear-Content $FDIR_LIST }
-  if (Test-Path $FONT_LIST) { Clear-Content $FONT_LIST }
+function Reset-FontStylesheet {
   if (Test-Path $FONT_WEBVIEW_CSS) { Clear-Content $FONT_WEBVIEW_CSS } 
-  # if (Test-Path $FONT_WEBVIEW_INDEX) { Clear-Content $FONT_WEBVIEW_INDEX } 
 }
 
-Clear-FontCache
+function Reset-FontCache {
 
-Get-ChildItem -Path $FONT_DIR -Recurse -Include *.otf, *.ttf | 
-Select-Object DirectoryName -Unique | 
-Export-Csv -Path $FDIR_LIST -NoTypeInformation
+  # if (Test-Path $FDIR_LIST) { Clear-Content $FDIR_LIST }
+  if (Test-Path $FONT_LIST) { Clear-Content $FONT_LIST }
+  
+  <# removed to resolve issues when editing with live server
+    if (Test-Path $FONT_WEBVIEW_INDEX) { Clear-Content $FONT_WEBVIEW_INDEX }
+  #>
 
-RemoveQuotesFromPath -File $FDIR_LIST
-
-$DIR = (Get-Content -Path $FDIR_LIST)
-foreach ($_path in $DIR) { 
-  Get-ChildItem -Path "$_path" -Recurse -Include *.otf, *.ttf | 
-  Select-Object FullName | 
-  Export-Csv -Path $FONT_LIST -Append -NoTypeInformation 
+  Write-Host "==> Scanning directories... " -NoNewline
+  Get-ChildItem -Path $Directory -Recurse -Include *.otf, *.ttf | 
+  Select-Object DirectoryName -Unique | 
+  Export-Csv -Path $FONT_LIST -NoTypeInformation
+  RemoveQuotesFromPath -File $FONT_LIST
+  Write-Host "Done."
+  
+  $DIR = (Get-Content -Path $FONT_LIST)
+  Clear-Content $FONT_LIST
+  Write-Host "==> Collecting fonts... " -NoNewline
+  foreach ($_path in $DIR) { 
+    Get-ChildItem -Path "$_path" -Recurse -Include *.otf, *.ttf | 
+    Select-Object FullName | 
+    Export-Csv -Path $FONT_LIST -Append -NoTypeInformation 
+  }
+  RemoveQuotesFromPath -File $FONT_LIST
+  Write-Host "Done."
 }
 
-RemoveQuotesFromPath -File $FONT_LIST
+if ($Update) { Reset-FontCache }
 
+Write-Host "==> Compiling webview... " -NoNewline
+Reset-FontStylesheet
 $FONT = (Get-Content -Path $FONT_LIST)
-
 Copy-Item ".\index.html" $FONT_WEBVIEW_INDEX
 
 foreach ($_font in $FONT) {
   $font_family = ($_font | Split-Path -Leaf).replace('.otf', '-OTF').replace('.ttf', '-TTF')
-  $font_url = $_font.replace($Directory,'').replace('\','/').substring(1)
-  
-  ( "@font-face { font-family: `"$font_family`"; src: url(`"$font_url`"); }" ) | 
+  $font_url = $_font.replace($Directory, '').replace('\', '/').substring(1)
+  ("@font-face{font-family:`"$font_family`";src:url(`"$font_url`");}") | 
   Out-File $FONT_WEBVIEW_CSS -Append -Encoding ascii
-
-  ("<option value=`"$font_family`">$font_family</option>") | Out-File $FONT_WEBVIEW_INDEX -Append  -Encoding ascii
+  ("<option value=`"$font_family`">$font_family</option>") | 
+  Out-File $FONT_WEBVIEW_INDEX -Append  -Encoding ascii
 }
 
+(Get-Content ".\index_end.html") | 
+Out-File $FONT_WEBVIEW_INDEX -Append -Encoding ascii
+Write-Host "Done."
+Write-Host "`nWebview location: $FONT_WEBVIEW_INDEX`n"
 
-(Get-Content ".\index_end.html") | Out-File $FONT_WEBVIEW_INDEX -Append -Encoding ascii
+exit
